6a45cff99a163da03577bd5de6b7657bd0c856b8
s4r44d43 <sararez@live.it>
2024-02-28 18:11:12 +0100

    [next step] control pari o dispari

--- js/script.js
+++ js/script.js
@@ -12,5 +12,46 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
+
+1. inizio con pari o dispari
+2. creo una variabile in cui salvo l'input html
+3. creo una variabile in cui salvo il valore dell'input
+4. creo una variabile dove salvo il div in cui stamperò il numero pc
+5. estraggo un numero random tramite funzione
+6. salvo la funzione del numero random pc dentro la variabile in cui ho preso il div
+7. stampo in html la variabile che contiene il div e il numero random
+8. creo una variabile dove salvo il div in cui andrà il risultato della somma
+9. con funzione controllo se è pari o dispari la somma 
+10. con if metto le condizioni per vincere
+11. stampo il vincitore
 */
 
+const numUser = document.getElementById('num-user');
+const userChoice = document.getElementById('user-choice');
+const numPc = document.querySelector('.num-pc');
+const esitoFinale = document.querySelector('.esito-finale');
+const TakeUserN = document.querySelector ('.take');
+
+function numeroPc (min, max) {
+
+ return Math.ceil(Math.random () * 5 );
+
+};
+
+TakeUserN.addEventListener('click', function() {
+
+  const numeroUser = parseInt(numUser.value);
+  console.log(numeroUser);
+
+  const userPoD = userChoice.value;
+  
+});
+
+const numeroRanPC = numeroPc (1, 5);
+
+  function somma (numeroUser, numeroPc) {
+
+  somma = numeroUser +  numeroPc;
+
+
+};
